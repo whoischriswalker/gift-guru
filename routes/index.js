8a10560fe6c8cef7f4cb1a4b2456c93a2af8f3ce
Chris Walker <cwalker12@gmail.com>
2022-11-25 10:02:21 -0800

    fixes

--- routes/index.js
+++ routes/index.js
@@ -3,26 +3,18 @@ var express = require('express')
 var router = express.Router()
 var items = require('../actions/items')
 
-function fnPageData(user,wishes,oldWishes,message) {
-  this.user = user
-  this.wishes = wishes
-  this.oldWishes = oldWishes
-  this.message = message
-}
-
 router.get('/', isLoggedIn, (req, res) => {
   const user = req.user
   const message = req.flash('addItemMessage') || req.flash('removeItemMessage')
 
-  const wishes = items.getWishList(req.user.userId, function (err, wishes) {
+  items.getWishList(req.user.userId, function (err, wishes) {
     if (err) throw err
-    return done(null,wishes)
+    pageData = new fnPageData(user,wishes,message)
   })
-  const oldWishes = items.getWishListHistory(req.user.userId, function (err, oldWishes) {
+/*  const oldWishes = items.getWishListHistory(req.user.userId, function (err, oldWishes) {
     if (err) throw err
     return done(null,oldWishes)
-  })
-  const pageData = new fnPageData(user,wishes,oldWishes,message)
+  })*/
   res.render('index', pageData)
 })
 
@@ -32,4 +24,11 @@ module.exports = router
 function isLoggedIn (req, res, next) {
   if (req.isAuthenticated()) { return next() }
   res.redirect('/login')
+}
+
+// create pageData object for logged in user
+function fnPageData(user,wishes,message) {
+  this.user = user
+  this.wishes = wishes
+  this.message = message
 }
